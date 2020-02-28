6ef8181adcb5501f09a7a6c5255f1efc490212f5
Andres Arizala <arizala13@gmail.com>
2020-02-27 19:42:10 -0600

    Adding form for journal entry - updating

--- lib/JournalEntryForm.dart
+++ lib/JournalEntryForm.dart
@@ -14,74 +14,74 @@ class _JournalEntryFormState extends State<JournalEntryForm>{
     return Material(
       child: Form(
         key: formKey,
-              child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Column(
           children: [
             new TextFormField(
-              autofocus: true,
-              decoration: InputDecoration(
-                labelText: 'Title', border: OutlineInputBorder() 
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: 'Title', border: OutlineInputBorder() 
+                  ),
+                  onSaved: (value) {
+                    // Store in some object 
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a Title';
+                    }
+                    else {
+                      return null;
+                    }
+                  },
                 ),
-                onSaved: (value) {
-                  // Store in some object 
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a title';
-                  }
-                  else {
-                    return null;
-                  }
-                },
-              ),
-              SizedBox(height: 10),
-              new TextFormField(
-              autofocus: true,
-              decoration: InputDecoration(
-                labelText: 'Body', border: OutlineInputBorder() 
+                SizedBox(height: 10),
+                new TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Body', border: OutlineInputBorder() 
+                  ),
+                  onSaved: (value) {
+                    // Store in some object 
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a Body';
+                    }
+                    else {
+                      return null;
+                    }
+                  },
                 ),
-                onSaved: (value) {
-                  // Store in some object 
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a Body';
-                  }
-                  else {
-                    return null;
-                  }
-                },
-              ),
-              SizedBox(height: 10),
+                SizedBox(height: 10),
         new TextFormField(
-              autofocus: true,
-              decoration: InputDecoration(
-                labelText: 'Rating', border: OutlineInputBorder() 
+                decoration: InputDecoration(
+                  labelText: 'Rating', border: OutlineInputBorder() 
+                  ),
+                  onSaved: (value) {
+                    // Store in some object 
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a Rating';
+                    }
+                    else {
+                      return null;
+                    }
+                  },
                 ),
-                onSaved: (value) {
-                  // Store in some object 
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a Rating';
-                  }
-                  else {
-                    return null;
-                  }
-                },
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                onPressed: () {
-                  if (formKey.currentState.validate()) {
-                    formKey.currentState.save();
-                    Navigator.of(context).pop();
-                  }
-                },
-                child: Text('Save Entry'),
-                ),
+                SizedBox(height: 10),
+                RaisedButton(
+                  onPressed: () {
+                    if (formKey.currentState.validate()) {
+                      formKey.currentState.save();
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: Text('Save Entry'),
+                  ),
         ],
       ),
+              ),
       )
       );
   }
